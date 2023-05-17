4834e04149837df7e5a67ec141a537e61cef551e
ubsefor <letzdropthebas@gmail.com>
2023-05-17 16:34:46 +0300

    Initial Commit

---
+++ DPLL-Solver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DPLL-Solver
+//
+//  Created by Alex Makhov on 17.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
